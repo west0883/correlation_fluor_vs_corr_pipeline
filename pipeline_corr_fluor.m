4befad4e23d3800a2a6b96b7c825ee196658aa43
Sarah West <west0883@umn.edu>
2023-01-17 08:15:27 -0600

    beginning of pipeline script

---
+++ pipeline_corr_fluor.m
@@ -0,0 +1,72 @@
+% pipeline_corr_fluor.m
+% Sarah West
+% 1/17/23
+% Runs correlations between results of PLSR on fluorescece and PLSR on correlaions
+% per node. Is to validate if average node correlation is dependent on node
+% fluorescence.
+
+%% Initial Setup  
+% Put all needed paramters in a structure called "parameters", which you
+% can then easily feed into your functions. 
+% Use correlations, Fisher transformed, mean removed within mice (mean
+% removed for at least the cases when you aren't using mice as response
+% variables).
+
+clear all; 
+
+% Create the experiment name.
+parameters.experiment_name='Random Motorized Treadmill';
+
+% Output directory name bases
+parameters.dir_base='Y:\Sarah\Analysis\Experiments\';
+parameters.dir_exper=[parameters.dir_base parameters.experiment_name '\']; 
+
+% Load mice_all, pass into parameters structure
+load([parameters.dir_exper '\mice_all.mat']);
+parameters.mice_all = mice_all;
+
+% ****Change here if there are specific mice, days, and/or stacks you want to work with**** 
+parameters.mice_all = parameters.mice_all;
+
+% Other parameters
+parameters.digitNumber = 2;
+parameters.yDim = 256;
+parameters.xDim = 256;
+parameters.number_of_sources = 32; 
+parameters.indices = find(tril(ones(parameters.number_of_sources), -1));
+
+% normal vs Warning periods, comparison types
+parameters.loop_variables.categories = {'normal', 'warningPeriods'};
+parameters.loop_variables.comparison_types = {'categorical', 'continuous'};
+
+% Load comparisons
+% normal vs warning period 
+for i = 1:numel(parameters.loop_variables.categories)
+    category = parameters.loop_variables.categories{i};
+    if strcmp(category, 'normal')
+        name = 'level1'; 
+    else 
+        name = category;
+    end
+    % categorical vs continuous
+    for typei = 1:numel(parameters.loop_variables.comparison_types)
+        type = parameters.loop_variables.comparison_types{typei};
+        load([parameters.dir_exper 'PLSR\comparisons_' name '_' type '.mat']);
+        parameters.(['comparisons_' type]).(category) = comparisons;
+        parameters.loop_variables.(['comparisons_' type]).(category) = parameters.(['comparisons_' type]);
+    end
+end
+
+clear comparisons name i type typei;
+
+% Names of all continuous variables.
+parameters.continuous_variable_names = {'speed', 'accel', 'duration', 'pupil_diameter'};
+
+% Put relevant variables into loop_variables.
+parameters.loop_variables.mice_all = parameters.mice_all;
+parameters.loop_variables.conditions = {'motorized'; 'spontaneous'};
+
+parameters.average_and_std_together = false;
+
+%% Start with categorical
+
