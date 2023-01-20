1c84f927d4ace6cbb951ddd26f3529ae0d815d6f
Sarah West <west0883@umn.edu>
2023-01-20 09:22:14 -0600

    function to calculate corr of corr vs fluor

---
+++ CorrFluor.m
@@ -0,0 +1,12 @@
+% CorrFluor.m
+% Sarah West
+% 1/20/23
+
+% High-level function to calculate correlation between changes in 
+% fluorescence and correlation per condition. 
+
+function [parameters] = CorrFluor(parameters)
+
+
+
+end 
