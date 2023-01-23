235fc901956c2260ec8a509fe918d99341f1b312
Sarah West <west0883@umn.edu>
2023-01-23 14:23:17 -0600

    continuous options

--- PadMissingMouse.m
+++ PadMissingMouse.m
@@ -10,20 +10,45 @@ function [parameters] = PadMissingMouse(parameters)
     % Inputs :
     % parameters.correlations -- 7 x 16 (mice x unique nodes)
     % parameters.fluorescence -- 7 x 16 (mice x unique nodes)
+    % parameters.mouseDim -- scalar; dimension of correlations and
+    % fluorescence the different mice are in.
+    % parameters.placement -- scalar; where in the order hte missing mouse
+    % should go
 
     % take these out becuase you might be altering them
     correlations = parameters.correlations;
     fluorescence = parameters.fluorescence;
+    mouseDim = parameters.mouseDim;
+
 
     % if mouse 1100 isn't there, 
-    if size(correlations,1) ~= parameters.number_of_mice
+    if size(correlations, mouseDim) ~= parameters.number_of_mice
 
-        % Put in a vector of NaNs where its data *should* go
-        correlations = [correlations(1:parameters.placement - 1, :); NaN(1, size(correlations, 2)); correlations(parameters.placement:end, :)];
-        fluorescence = [fluorescence(1:parameters.placement - 1, :); NaN(1, size(fluorescence, 2)); fluorescence(parameters.placement:end, :)];
+        sizes = size(correlations);
+
+        % Create matrices for variying dimension indexing. 
+        Cin= repmat({':'}, 1, numel(sizes));
+        Cout = repmat({':'}, 1, numel(sizes));
+
+        sizes = size(correlations);
+        sizes_new = sizes;
+        sizes_new(mouseDim) = sizes(mouseDim) + 1;
+
+        correlations_padded  = NaN(sizes_new);
+        fluorescence_padded = NaN(sizes_new);
+
+        % Put in data
+        Cout{mouseDim} = [1:parameters.placement - 1, parameters.placement + 1:sizes_new(mouseDim)]; 
+      
+        correlations_padded(Cout{:}) = correlations; 
+        fluorescence_padded(Cout{:}) = fluorescence;
+        
+    else 
+        correlations_padded = correlations;
+        fluorescence_padded = fluorescence;
     end 
 
-    parameters.corrs_padded = correlations;
-    parameters.fluors_padded = fluorescence;
+    parameters.corrs_padded = correlations_padded;
+    parameters.fluors_padded = fluorescence_padded;
 
 end 
